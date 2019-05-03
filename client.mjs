f6ad6796f5053ce1dcf002411c31f7ead311977e
Prakkmak <Prakkmak@gmail.com>
2019-05-03 12:50:30 +0200

    Fix SetComponentVariation

--- client.mjs
+++ client.mjs
@@ -10,35 +10,35 @@ const weapons = [
     "WEAPON_ASSAULTRIFLE", "WEAPON_CARBINERIFLE",
     "WEAPON_PUMPSHOTGUN"
 ];
-const keys = 
+const keys =
 {
-    'L' : 76,
-    'K' : 75,
-    'U' : 85,
-    'E' : 69,
-    'A' : 65,
-    'T' : 84,
-    'Esc' : 27,
-    'Enter' : 13,
-    'Numpad_0' : 96,
-    'Numpad_1' : 97,
-    'Numpad_2' : 98,
-    'Numpad_3' : 99,
-    'Numpad_4' : 100,
-    'Numpad_5' : 101,
-    'Numpad_6' : 102,
-    'Numpad_7' : 103,
-    'Numpad_8' : 104,
-    'Numpad_9' : 105,
-    '=' : 187,
-    'power2' : 222
+    'L': 76,
+    'K': 75,
+    'U': 85,
+    'E': 69,
+    'A': 65,
+    'T': 84,
+    'Esc': 27,
+    'Enter': 13,
+    'Numpad_0': 96,
+    'Numpad_1': 97,
+    'Numpad_2': 98,
+    'Numpad_3': 99,
+    'Numpad_4': 100,
+    'Numpad_5': 101,
+    'Numpad_6': 102,
+    'Numpad_7': 103,
+    'Numpad_8': 104,
+    'Numpad_9': 105,
+    '=': 187,
+    'power2': 222
 };
-const ped_bones = 
+const ped_bones =
 {
-    'IK_Head' : 0x322c,
-    'SKEL_Head ' : 0x796E,
-    'FACIAL_facialRoot' : 0xFE2C, 
-    'SKEL_Spine2' : 0x60f1,
+    'IK_Head': 0x322c,
+    'SKEL_Head ': 0x796E,
+    'FACIAL_facialRoot': 0xFE2C,
+    'SKEL_Spine2': 0x60f1,
     'SKEL_Root': 0x0
 };
 var elems = ["masque", "cheveux", "torse", "jambe", "sac", "pieds", "accessoire", "sous-haut", "armure", "détail", "haut"];
@@ -51,39 +51,37 @@ let animationsView = null;
 //#endregion
 let cursorVisible = false;
 
-class SpotLight
-{
+class SpotLight {
 
-    constructor(_x,_y,_z) 
-    {
-        this.position = 
-        {
-            x:_x,
-            y:_y,
-            z:_z
-        };
-        this.direction = 
-        {
-            x:0,
-            y:0,
-            z:0
-        };
+    constructor(_x, _y, _z) {
+        this.position =
+            {
+                x: _x,
+                y: _y,
+                z: _z
+            };
+        this.direction =
+            {
+                x: 0,
+                y: 0,
+                z: 0
+            };
         this.color =
-        {
-            r: 255,
-            g: 0,
-            b: 255
-        };
+            {
+                r: 255,
+                g: 0,
+                b: 255
+            };
         this.brightness = 100;
         this.distance = 255;
         this.hardness = 0;
         this.radius = 13;
     }
 };
-const rgbToHex = function (rgb) { 
+const rgbToHex = function (rgb) {
     var hex = Number(rgb).toString(16);
     if (hex.length < 2) {
-         hex = "0" + hex;
+        hex = "0" + hex;
     }
     return hex;
 };
@@ -95,16 +93,13 @@ let webviewLayer = 0;
 /* Init */
 start();
 
-alt.onServer('setComponent', (argsDrawable, argsTexture, argsPalette) => 
-{
+alt.onServer('setComponent', (argsDrawable, argsTexture, argsPalette) => {
     setComponentVariation(argsDrawable, argsTexture, argsPalette);
 });
-alt.onServer('setHeadOverlay', (argsIndex, argsOpacity) => 
-{
+alt.onServer('setHeadOverlay', (argsIndex, argsOpacity) => {
     setHeadOverlay(argsIndex, argsOpacity);
 });
-alt.onServer('setProps', (argsDrawable, argsTexture) => 
-{
+alt.onServer('setProps', (argsDrawable, argsTexture) => {
     setProps(argsDrawable, argsTexture);
 });
 alt.onServer('setFreeze', (value) => {
@@ -113,7 +108,7 @@ alt.onServer('setFreeze', (value) => {
 alt.onServer('giveAllWeapons', () => {
     giveAllWeapons();
 });
-alt.onServer('setWorldTime', (h,m,s) => {
+alt.onServer('setWorldTime', (h, m, s) => {
     alt.setClockTime(h, m, s);
 });
 alt.onServer('loadIpl', (ipl) => {
@@ -125,8 +120,7 @@ alt.onServer('unloadIpl', (ipl) => {
     alt.log("IPL unloaded > " + ipl);
 });
 alt.onServer('openView', (viewName) => {
-    if(viewName == 'characterCustom')
-    {
+    if (viewName == 'characterCustom') {
         openCharacterCustom();
     }
 });
@@ -145,34 +139,28 @@ alt.on('keydown', (key) => {
     if (key == keys.Enter || key == keys.Esc) {
         chatopen = false;
     }
-    if(key == keys.Esc)
-    {
-        
+    if (key == keys.Esc) {
+
         closeAnimationsView();
     }
-    if(key == keys['='])
-    {
+    if (key == keys['=']) {
         openAnimationsView();
     }
-    if(!chatopen)
-    {
-        if(skinenabled)
-        {
+    if (!chatopen) {
+        if (skinenabled) {
             vetementChanger(key);
-        } else if(key == keys.power2)
-        {
+        } else if (key == keys.power2) {
             skinenabled = !skinenabled;
         }
     }
 });
 
-function start()
-{
+function start() {
     setModel("Male");
     goBackToGameplayCam();
     giveAllWeapons();
     alt.emitServer("setlicense", alt.getLicenseHash());
-    
+
     alt.initVoice();
     alt.setMicGain(1);
     alt.log("Voice on");
@@ -193,72 +181,56 @@ function giveAllWeapons() {
  *
  * @param {boolean}   value           Est bloqué ou non.
  */
-function setControlsEnabled(value)
-{
-    if(alt.gameControlsEnabled != value)
-    {
+function setControlsEnabled(value) {
+    if (alt.gameControlsEnabled != value) {
         alt.toggleGameControls(value);
     }
 }
-function freezePlayer(value)
-{
-    game.freezeEntityPosition(game.playerPedId(),value);
+function freezePlayer(value) {
+    game.freezeEntityPosition(game.playerPedId(), value);
     setControlsEnabled(value);
 }
-function setCursorVisible(value)
-{
-    if(cursorVisible != value)
-    {
+function setCursorVisible(value) {
+    if (cursorVisible != value) {
         alt.showCursor(value);
         cursorVisible = value;
     }
 }
 
-function setModel(model)
-{
-    if(model.toLowerCase() == "male")
-    {
+function setModel(model) {
+    if (model.toLowerCase() == "male") {
         model = game.getHashKey("mp_m_freemode_01");
-    } else if(model.toLowerCase() == "female") {
-        model = game.getHashKey("mp_f_freemode_01");  
+    } else if (model.toLowerCase() == "female") {
+        model = game.getHashKey("mp_f_freemode_01");
     } else {
         model = game.getHashKey(model);
     }
 
     game.requestModel(model);
-        
+
     alt.setTimeout(() => {
         game.setPlayerModel(game.playerId(), model);
         game.setPedDefaultComponentVariation(game.playerPedId());
         game.setPedHeadBlendData(game.playerPedId(), 0, 0, 0, 0, 0, 0, 0, 0, 0, true);
     }, 100);
 }
-function setHairColor(colorID, highlightColorID)
-{
+function setHairColor(colorID, highlightColorID) {
     alt.log("setHairColor " + colorID);
     game.setPedHairColor(game.playerPedId(), colorID, highlightColorID);
 }
-function setHeadOverlay(argsIndex, argsOpacity)
-{
-    for (var i = 0; i < 13; i++) 
-    {
-        if (argsIndex[i] != null && argsOpacity[i] != null)
-        {
-            game.setPedHeadOverlay(game.playerPedId(), i , argsIndex[i], argsOpacity[i]);
+function setHeadOverlay(argsIndex, argsOpacity) {
+    for (var i = 0; i < 13; i++) {
+        if (argsIndex[i] != null && argsOpacity[i] != null) {
+            game.setPedHeadOverlay(game.playerPedId(), i, argsIndex[i], argsOpacity[i]);
         }
     }
 }
-function setHeadOverlayColor(argsColor)
-{
-    for(let i=0;i<13;i++)
-    {
-        if (argsColor[i] != null)
-        {
-            if(i == 1 || i == 10 || i == 2)
-            {
+function setHeadOverlayColor(argsColor) {
+    for (let i = 0; i < 13; i++) {
+        if (argsColor[i] != null) {
+            if (i == 1 || i == 10 || i == 2) {
                 game.setPedHeadOverlayColor(game.playerPedId(), i, 1, argsColor[i], argsColor[i]);
-            } else if(i == 5 || i == 8) 
-            {
+            } else if (i == 5 || i == 8) {
                 game.setPedHeadOverlayColor(game.playerPedId(), i, 2, argsColor[i], argsColor[i]);
             } else {
                 game.setPedHeadOverlayColor(game.playerPedId(), i, 0, argsColor[i], argsColor[i]);
@@ -266,38 +238,30 @@ function setHeadOverlayColor(argsColor)
         }
     }
 }
-function setProps(argsDrawable, argsTexture)
-{
-    for (var i = 0; i < 3; i++) 
-    {
-        if (argsDrawable[i] != null && argsTexture[i] != null)
-        {
-            game.setPedPropIndex(game.playerPedId(), prodComponentIds[i] , argsDrawable[i], argsTexture[i], true);
+function setProps(argsDrawable, argsTexture) {
+    for (var i = 0; i < 3; i++) {
+        if (argsDrawable[i] != null && argsTexture[i] != null) {
+            game.setPedPropIndex(game.playerPedId(), prodComponentIds[i], argsDrawable[i], argsTexture[i], true);
         }
     }
 }
-function setComponentVariation(argsDrawable, argsTexture, argsPalette)
-{
-    for (var i = 0; i < 12; i++) 
-    {
-        if (argsDrawable[i] != null && argsTexture[i] != null && argsPalette[i] != null)
-        {
-            game.setPedComponentVariation(game.playerPedId(), i, argsDrawable[i], argsTexture[i], argsPalette[i]);
-        }
+function setComponentVariation(argsDrawable, argsTexture, argsPalette) {
+
+    if (argsDrawable == null) argsDrawable = new Array(12).fill(0);
+    if (argsTexture == null) argsTexture = new Array(12).fill(0);
+    if (argsPalette == null) argsPalette = new Array(12).fill(0);
+    for (var i = 0; i < 12; i++) {
+        game.setPedComponentVariation(game.playerPedId(), i + 1, argsDrawable[i], argsTexture[i], argsPalette[i]);
     }
 }
-function setFaceFeature(argsIndex, argsScale)
-{
-    for(let i=0;i<20;i++)
-    {
+function setFaceFeature(argsIndex, argsScale) {
+    for (let i = 0; i < 20; i++) {
         game.setPedFaceFeature(game.playerPedId(), key, value);
     }
 }
-function requestHairColors(web, id, container, size, callback)
-{
+function requestHairColors(web, id, container, size, callback) {
     let colors = new Array(game.getNumHairColors());
-    for(let i=0;i<game.getNumHairColors();i++)
-    {
+    for (let i = 0; i < game.getNumHairColors(); i++) {
         let [_, r, g, b] = game.getHairColor(i, 0, 0, 0);
         colors[i] = "'#" + rgbToHex(r) + rgbToHex(g) + rgbToHex(b) + "'";
     }
@@ -311,27 +275,22 @@ function requestHairColors(web, id, container, size, callback)
  *
  * @param {WebView}   web           La WebView à focus.
  */
-function setFocusOn(web)
-{
+function setFocusOn(web) {
     web.focus();
 }
-function isWebViewOpened(web)
-{
+function isWebViewOpened(web) {
     return web != null;
 }
-function destroyWebView(web)
-{
+function destroyWebView(web) {
     web.destroy();
 }
 
-function openCharacterCustom()
-{
-    if(!isWebViewOpened(characterCustomView))
-    {
+function openCharacterCustom() {
+    if (!isWebViewOpened(characterCustomView)) {
         characterCustomView = new alt.WebView("http://resources/lambda-client/client/html/charactercustom/charactercustom.html");
 
-        characterCustomView.on('setHairColor', (colorID,highlightColorID) => {
-            
+        characterCustomView.on('setHairColor', (colorID, highlightColorID) => {
+
             setHairColor(colorID, highlightColorID);
         });
         characterCustomView.on('setHeadOverlay', (key, index, opacity) => {
@@ -342,7 +301,7 @@ function openCharacterCustom()
             arg2[key] = opacity;
 
             setHeadOverlay(arg1, arg2);
-        }); 
+        });
         characterCustomView.on('setComponent', (key, drawable, texture, palette) => {
             let arg1 = new Array(11).fill(null);
             let arg2 = new Array(11).fill(null);
@@ -352,7 +311,7 @@ function openCharacterCustom()
             arg2[key] = texture;
             arg3[key] = palette;
 
-            setComponentVariation(arg1,arg2,arg3);
+            setComponentVariation(arg1, arg2, arg3);
         });
         characterCustomView.on('setFaceFeature', (key, value) => {
             let arg = new Array(20).fill(null);
@@ -360,24 +319,22 @@ function openCharacterCustom()
 
             game.setPedFaceFeature(game.playerPedId(), key, value);
         });
-        characterCustomView.on('requestHairColors', (id, container,size,callback) => {
+        characterCustomView.on('requestHairColors', (id, container, size, callback) => {
             requestHairColors(characterCustomView, id, container, size, callback);
         });
         characterCustomView.on('camFocusBodypart', (bodypart, offset, fov, easeTime) => {
-            focusOnBone(bodypart,offset, fov, easeTime);
+            focusOnBone(bodypart, offset, fov, easeTime);
         });
         characterCustomView.on('setModel', (model) => {
             setModel(model);
         });
-        
+
         webviewLayer++;
         setFocusOn(characterCustomView);
     }
 }
-function closeCharacterCustom()
-{
-    if(isWebViewOpened(characterCustomView)) 
-    {
+function closeCharacterCustom() {
+    if (isWebViewOpened(characterCustomView)) {
         destroyWebView(characterCustomView);
         characterCustomView = null;
         webviewLayer--;
@@ -385,10 +342,8 @@ function closeCharacterCustom()
     }
 }
 
-function openContextView()
-{
-    if(!isWebViewOpened(contextView)) 
-    {
+function openContextView() {
+    if (!isWebViewOpened(contextView)) {
         contextView = new alt.WebView('http://resources/lambda-client/client/html/context/context.html')
 
         contextView.on('chatmessage', (text) => {
@@ -401,10 +356,8 @@ function openContextView()
     //#region event_context
     //#endregion
 }
-function closeContextView()
-{
-    if(isWebViewOpened(contextView))    
-    {
+function closeContextView() {
+    if (isWebViewOpened(contextView)) {
         destroyWebView(contextView);
         contextView = null;
         webviewLayer--;
@@ -412,10 +365,8 @@ function closeContextView()
 
 }
 
-function openSkinChangerView()
-{
-    if(!isWebViewOpened(skinchangerView))
-    {
+function openSkinChangerView() {
+    if (!isWebViewOpened(skinchangerView)) {
         skinchangerView = new alt.WebView('http://resources/lambda-client/client/html/skinchanger/skinchanger.html');
 
         skinchangerView.on('chatmessage', (text) => {
@@ -423,15 +374,13 @@ function openSkinChangerView()
             alt.emitServer('chatmessage', text);
         });
         skinchangerView.on('skinchange', (text) => {
-            alt.emitServer('chatmessage', "!vetements "+text);
+            alt.emitServer('chatmessage', "!vetements " + text);
         });
         webviewLayer++;
     }
 }
-function closeSkinChangerView()
-{
-    if(isWebViewOpened(skinchangerView))    
-    {
+function closeSkinChangerView() {
+    if (isWebViewOpened(skinchangerView)) {
         destroyWebView(skinchangerView);
         skinchangerView = null;
         webviewLayer--;
@@ -439,10 +388,8 @@ function closeSkinChangerView()
 
 }
 
-function openAnimationsView()
-{
-    if(!isWebViewOpened(animationsView))
-    {
+function openAnimationsView() {
+    if (!isWebViewOpened(animationsView)) {
         animationsView = new alt.WebView('http://resources/lambda-client/client/html/animations/animations.html');
 
         animationsView.on('load', () => {
@@ -450,7 +397,7 @@ function openAnimationsView()
                 animationsView.execJS(`add_buttonanim('${animation.anims[anim].label}', '${anim}')`);
             });
         });
-        
+
         animationsView.on('playAnim', (anim) => {
             animation.playAnim(anim);
         });
@@ -459,10 +406,8 @@ function openAnimationsView()
         webviewLayer++;
     }
 }
-function closeAnimationsView()
-{
-    if(isWebViewOpened(animationsView))    
-    {
+function closeAnimationsView() {
+    if (isWebViewOpened(animationsView)) {
         destroyWebView(animationsView);
         animationsView = null;
         webviewLayer--;
@@ -471,8 +416,7 @@ function closeAnimationsView()
 }
 //#endregion
 //#region KEY CONTROLS
-function vetementChanger(key)
-{
+function vetementChanger(key) {
     if (key == 102) {
         alt.emitServer('chatmessage', "/vetement suivant " + selected);
     }
@@ -513,8 +457,7 @@ function vetementChanger(key)
 }
 //#endregion
 //#region CAM FUNCTIONS
-function createCam(position, rotation, fov)
-{
+function createCam(position, rotation, fov) {
     alt.nextTick(() => {
         cam = game.createCam('DEFAULT_SCRIPTED_CAMERA', false);
         game.setCamCoord(cam, position.x, position.y, position.z);
@@ -525,15 +468,13 @@ function createCam(position, rotation, fov)
 
     return cam;
 }
-function focusOnBone(bone, offset, fov, easeTime)
-{
-    bone = (typeof(bone) == 'string') ? ped_bones[bone] : bone;
-    if(cam == null) 
-    {
-        createCam({x: 0, y:0, z:0}, {x: 0, y:0, z:0}, fov);
+function focusOnBone(bone, offset, fov, easeTime) {
+    bone = (typeof (bone) == 'string') ? ped_bones[bone] : bone;
+    if (cam == null) {
+        createCam({ x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, fov);
         alt.nextTick(() => {
             game.attachCamToPedBone(cam, game.playerPedId(), bone, offset.x, offset.y, offset.z, true);
-            game.pointCamAtPedBone(cam, game.playerPedId(), bone, 0, 0, 0, true);    
+            game.pointCamAtPedBone(cam, game.playerPedId(), bone, 0, 0, 0, true);
             game.renderScriptCams(true, true, easeTime, true, false);
         });
     } else {
@@ -542,31 +483,28 @@ function focusOnBone(bone, offset, fov, easeTime)
             game.renderScriptCams(true, true, easeTime, true, false);
             game.setCamFov(cam, fov);
             game.attachCamToPedBone(cam, game.playerPedId(), bone, offset.x, offset.y, offset.z, true);
-            game.pointCamAtPedBone(cam, game.playerPedId(), bone, 0, 0, 0, true);    
+            game.pointCamAtPedBone(cam, game.playerPedId(), bone, 0, 0, 0, true);
         });
     }
 }
-function goBackToGameplayCam()
-{
-    
+function goBackToGameplayCam() {
+
     alt.nextTick(() => {
 
         game.setCamActive(cam, false);
         game.destroyAllCams(false);
-        cam=null;
+        cam = null;
         game.renderScriptCams(false, false, 0, true, true);
     });
-    
+
 }
 //#endregion
 //#region LIGHT FUNCTIONS
 
-function createLight(id)
-{
+function createLight(id) {
     spotlights[id] = new SpotLight(game.getEntityCoords(game.playerPedId()).x, game.getEntityCoords(game.playerPedId()).y, game.getEntityCoords(game.playerPedId()).z);
 }
-function renderLights()
-{
+function renderLights() {
     spotlights.forEach((light) => {
         game.drawSpotLight(light.position.x, light.position.y, light.position.z, light.direction.x, light.direction.y, light.direction.z, light.color.r, light.color.g, light.color.b, light.distance, light.brightness, light.hardness, light.radius, 1);
     });
@@ -574,19 +512,16 @@ function renderLights()
 
 //#endregion
 //#region IPL FUNCTIONS
-function loadIPL(ipl)
-{
+function loadIPL(ipl) {
     game.RequestIpl(ipl);
 }
-function unloadIPL(ipl)
-{
+function unloadIPL(ipl) {
     game.removeIpl(ipl);
 }
 //#endregion
 
 alt.on('update', () => {
-    if(webviewLayer > 0)
-    {
+    if (webviewLayer > 0) {
         setControlsEnabled(false);
         setCursorVisible(true);
     } else {
