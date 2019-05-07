cc77233341dd26051d0e29779d96f4442c132719
tttt <ttt>
2019-05-08 01:01:48 +0200

    ATM CEF + edit rapportbug

--- client/html/rapportbug/app.js
+++ client/html/rapportbug/app.js
@@ -1,9 +1,11 @@
 function bug() {
-    var texte = document.getElementById('texte').value;
-    var categorie = document.querySelector('input[name="categorie"]:checked').value;
-    var importance = document.querySelector('input[name="importance"]:checked').value;
+  var texte = document.getElementById("texte").value;
+  var categorie = document.querySelector('input[name="categorie"]:checked')
+    .value;
+  var importance = document.querySelector('input[name="importance"]:checked')
+    .value;
 
-    var phrase = importance + "," + categorie + "," + texte;
+  var phrase = "/bug" + importance + "," + categorie + "," + texte;
 
-    alt.emitServer('chatmessage', phrase);
+  alt.emitServer("chatmessage", phrase);
 }
