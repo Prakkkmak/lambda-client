9147c875d0c9a59c942c4658755be6d0d5706a59
tttt <ttt>
2019-05-08 15:54:18 +0200

    Ticket CEF + edit rapportbug

--- client/html/ticket/app.js
+++ client/html/ticket/app.js
@@ -1,11 +1,9 @@
 function bug() {
   var texte = document.getElementById("texte").value;
-  var categorie = document.querySelector('input[name="categorie"]:checked')
-    .value;
   var importance = document.querySelector('input[name="importance"]:checked')
     .value;
 
-  var phrase = "/bug " + importance + ", " + categorie + ", " + texte;
+  var phrase = "/ticket " + importance + ", " + texte;
 
   alt.emitServer("chatmessage", phrase);
 }
