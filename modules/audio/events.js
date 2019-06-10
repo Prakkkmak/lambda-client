a98b2d27011aa727962b46f84cbdb28727a46456
Nauwp <noepec1@gmail.com>
2019-06-10 18:46:18 +0200

    Fix audio event

--- modules/audio/events.js
+++ modules/audio/events.js
@@ -3,5 +3,5 @@ import * as alt from 'alt';
 import * as audio from 'modules/audio/main';
 
 alt.onServer('playSoundCoord', (audioName, audioRef, posX, posY, posZ, range) => {
-    audio.playSoundFromCoord(audioName, audioRef, posX, posY, posZ, range);
+    audio.playSoundFromCoord(audioName, audioRef, {x:posX, y:posY, z:posZ}, range);
 });
